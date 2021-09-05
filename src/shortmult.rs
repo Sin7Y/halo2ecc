e0f54141ad989bcd694e6800062f009949fa2850
Xin Gao <xgao@zoyoe.com>
2021-09-06 00:08:21 +0800

    sketch out shortmult.rs

--- src/shortmult.rs
+++ src/shortmult.rs
@@ -2,37 +2,39 @@ extern crate halo2;
 
 use std::marker::PhantomData;
 use crate::types::Number;
+use std::convert::TryFrom;
 
 use halo2::{
     arithmetic::FieldExt,
     circuit::{Cell, Chip, Layouter, Region, SimpleFloorPlanner},
-    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Instance, TableColumn, Selector},
     poly::Rotation,
 };
 
-trait ShortMult <F: FieldExt>: Chip<F> {
-    /// Variable representing a number.
-    type Num;
+// Short Multiplication for a * b \in [2 ^ (16 * d) .. 2 ^ (16 * c)]
+// let c = a * b then after decompose c into 16 bits as [x_i] we need to lookup table to
+// calculate x_i' and then sum x_i to get the final result
 
-    fn perform(
+#[derive(Clone, Debug)]
+struct ShortMultConfig {
+    decompose: Column<Advice>,
+    shift: Column<Advice>,
+    out: Column<Advice>,
+    sel: Selector,
+}
+
+trait ShortMult <F: FieldExt>: Chip<F> {
+    fn mult (
         &self,
         layouter: impl Layouter<F>,
-        a: Self::Num,
-        b: Self::Num,
-    ) -> Result<Self::Num, Error>;
+        a: Number<F>,
+        b: Number<F>,
+    ) -> Result<Number<F>, Error>;
 }
 
 struct ShortMultChip<F: FieldExt> {
     config: ShortMultConfig,
     _marker: PhantomData<F>,
-}
-
-#[derive(Clone, Debug)]
-struct ShortMultConfig {
-    /// Two fp numbers, three Columns each
-    advice: [Column<Advice>; 2],
-    carry: Column<Instance>,
-    sel: Selector,
 }
 
 impl<F: FieldExt> ShortMultChip<F> {
@@ -45,18 +47,56 @@ impl<F: FieldExt> ShortMultChip<F> {
 
     fn configure(
         meta: &mut ConstraintSystem<F>,
+        lookup_v: TableColumn,
     ) -> <Self as Chip<F>>::Config {
-        let advice = [
-          meta.advice_column(),
-          meta.advice_column(),
-        ];
-        let carry = meta.instance_column();
+        let decompose = meta.advice_column();
+        let shift = meta.advice_column();
+        let out = meta.advice_column();
         let sel = meta.selector();
         ShortMultConfig {
-            advice,
-            carry,
+            decompose,
+            shift,
+            out,
             sel,
         }
+    }
+
+    fn assign_region(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        low: bool, // high 128 bits or low 128 bits
+        chunk_size: usize,
+        shift_start: usize,
+    ) -> Result<(), Error> {
+        let pend = F::from(0);
+        let config = self.config();
+        layouter.assign_region(
+            || "load private",
+            |mut region| {
+                for p in 0 .. chunk_size {
+                    let s:u64 = if p < shift_start { 0 } else {
+                        u64::try_from((p - shift_start)).unwrap()
+                    };
+                    let decompose_cell = region.assign_advice(
+                        || format!("d_{}", p),
+                        config.decompose,
+                        p,
+                        || Ok(pend),
+                    )?;
+                    let shift = region.assign_advice(
+                        || format!("operand_{}", p),
+                        config.shift,
+                        p,
+                        || Ok(F::from_u64(s)),
+                    )?;
+                    //region.constrain_equal(e.cell, cell)?;
+                    //sum = sum + e.value.unwrap();
+                    // missing selector enabling
+                }
+                Ok(())
+            },
+        )?;
+        Ok(())
     }
 }
 
@@ -74,14 +114,12 @@ impl<F: FieldExt> Chip<F> for ShortMultChip<F> {
 }
 
 impl<F: FieldExt> ShortMult<F> for ShortMultChip<F> {
-    type Num = Number<F>;
-
-    fn perform(
+    fn mult(
         &self,
         mut layouter: impl Layouter<F>,
-        a: Self::Num,
-        b: Self::Num,
-    ) -> Result<Self::Num, Error> {
+        a: Number<F>,
+        b: Number<F>,
+    ) -> Result<Number<F>, Error> {
         let mut out = None;
         Ok(out.unwrap())
     }
